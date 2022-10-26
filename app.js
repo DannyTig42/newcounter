fc610979d970d428539af1a7fb661cbf3bc99e70
Daniel <igbemao@gmail.com>
2022-10-26 16:15:39 +0100

    bgrc add

--- app.js
+++ app.js
@@ -4,19 +4,26 @@ let res = document.querySelector('.res')
 let dec = document.querySelector('.dec')
 
 function DECREASE (){
+    let body = document.body
+    body.style.backgroundColor = 'yellow'
     heading.innerText--
+    
 }
 
 inc.addEventListener('click',DECREASE)
 
 function REST (){
+    let body = document.body
     heading.innerText = 0
+    body.style.backgroundColor = 'red'
 }
 
 res.addEventListener('click',REST)
 
 function INCREASE (){
+    let body = document.body
     heading.innerText++
+    body.style.backgroundColor = 'green'
 }
 
 dec.addEventListener('click',INCREASE)
